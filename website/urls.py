00be1db3f3b29b31b3e86e3702edf8c5c6bde13b
18catt <98676305+18catt@users.noreply.github.com>
2024-03-25 04:04:09 +0530

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'website'
 
 urlpatterns = [
+    #This path will correspond to link http://127.0.0.1:8000 and go to views file and look for home function(for function-based program) or class home(for class-based program)
     path('', views.home, name='homepage'),
     path('about', views.about, name = 'aboutpage'),
     path('more', views.more, name = 'morepage')
