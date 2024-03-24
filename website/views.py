8c3f02dce611e7a47888e81cffe10eb54ccb0b6f
18catt <98676305+18catt@users.noreply.github.com>
2024-03-25 04:04:49 +0530

    Update views.py

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    #This command will render the request and fetch the given template file
     return render(request, 'index.html')
 
 def about(request):
